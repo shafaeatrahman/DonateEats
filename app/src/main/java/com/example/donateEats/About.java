7f9b2650e6bb29925794bc662f4e26390ef1ae8b
shafaeatrahman <shafaeatrahman2@gmail.com>
2023-09-20 23:03:54 +0600

    second commit

---
+++ app/src/main/java/com/example/donateEats/About.java
@@ -0,0 +1,55 @@
+package com.example.donateEats;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+public class About extends AppCompatActivity {
+
+    // Declare CardView elements
+    CardView instagram, facebook, twitter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        // Initialize CardView elements by finding their IDs in the layout
+        instagram = findViewById(R.id.instagram);
+        facebook = findViewById(R.id.facebook);
+        twitter = findViewById(R.id.twitter);
+
+        // Set up onClick listeners for each CardView
+        instagram.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Open Instagram link in a web browser
+                Intent myWebLink = new Intent(android.content.Intent.ACTION_VIEW);
+                myWebLink.setData(Uri.parse("http://www.instagram.com"));
+                startActivity(myWebLink);
+            }
+        });
+        facebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Open Facebook link in a web browser
+                Intent myWebLink = new Intent(android.content.Intent.ACTION_VIEW);
+                myWebLink.setData(Uri.parse("http://www.facebook.com"));
+                startActivity(myWebLink);
+            }
+        });
+        twitter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Open Twitter link in a web browser
+                Intent myWebLink = new Intent(android.content.Intent.ACTION_VIEW);
+                myWebLink.setData(Uri.parse("http://www.twitter.com"));
+                startActivity(myWebLink);
+            }
+        });
+    }
+}
